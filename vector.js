753e55ebf1f9292edf96837a1c50aa14045ce6aa
Alexander Mihalicyn <alexander@mihalicyn.ru>
2011-11-24 21:10:55 +0600

    fix

--- vector.js
+++ vector.js
@@ -19,9 +19,9 @@ function vecsmul( v1, v2 ) {
 }
 
 function vecvmul( v1, v2 ) {
-	x = v1[1]*v2[2] - v1[2]*v2[1];
-	y = v1[2]*v2[0] - v1[0]*v2[2];
-	z = v1[0]*v2[1] - v1[1]*v2[0];
+	var x = v1[1]*v2[2] - v1[2]*v2[1];
+	var y = v1[2]*v2[0] - v1[0]*v2[2];
+	var z = v1[0]*v2[1] - v1[1]*v2[0];
 	return [ x, y, z ];
 }
 
